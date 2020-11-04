42b6788ef04effcfb4dd7c24e628f7387e1f4150
rayast17 <72138164+rayast17@users.noreply.github.com>
2020-11-04 15:08:51 +0200

    Add files via upload

---
+++ palindromicprimemethod.java
@@ -0,0 +1,48 @@
+package domrab;
+public class palindromicprimemethod {
+
+	public static void main(String[] args) {
+		System.out.println(2 + "\t");
+		int count = 1;
+		int number = 3;
+		
+		while (count < 120) {
+			if (! isPalindrom(number) || ! isPrime(number)) {
+				number++;
+				continue;
+			}
+		
+			System.out.print(number + "\t");
+			count++;
+			number++;
+			if(count % 10 == 0) {
+				System.out.println();
+			}
+		}
+	}
+	public static boolean isPrime(int aNumber) {
+		int maxDivider = (int) Math.ceil(Math.sqrt(aNumber));
+		boolean isPrime = true;
+		for (int i = 2; i <= maxDivider; i++) {
+			if (aNumber % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}	
+		return isPrime;
+	}
+
+
+public static boolean isPalindrom(int aNumber) {
+	int copyNumber = aNumber;
+	int reversedNumber = 0;
+	while (copyNumber != 0) {
+		
+		int remainder = copyNumber % 10;
+		reversedNumber = reversedNumber * 10 + remainder;
+		copyNumber /= 10;
+		
+	}
+	return aNumber == reversedNumber;
+}
+}
