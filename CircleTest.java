58c1a5d4a7f929366e3cc9b3496a5a13fa8818e8
rayast17 <72138164+rayast17@users.noreply.github.com>
2020-11-19 17:54:49 +0200

    Add files via upload

---
+++ CircleTest.java
@@ -0,0 +1,17 @@
+package domrab;
+
+public class CircleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c1 = new Circle();
+		System.out.println("Area of c1 "+ c1.getArea() + " Perimeter of c1 "+ c1.getPerimeter() );
+		
+		Circle c2 = new Circle(5);
+		System.out.println("Area of c2 "+ c2.getArea() + " Perimeter of c2 "+ c2.getPerimeter() );
+		
+		c1.setRadius(10);
+		System.out.println("Area of c1 "+ c1.getArea() + " Perimeter of c1 "+ c1.getPerimeter() );
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,15 @@
+package domrab;
+
+public class Student {
+	public String name;
+	public double grade;
+	
+	public Student() {
+		
+	}
+	
+	public Student (String name, double grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+}
